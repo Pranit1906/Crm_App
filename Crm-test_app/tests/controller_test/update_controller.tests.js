8b6f3cbc5f3d1534f77eaa2ecd8b8bbbe6059dd8
unknown <pranitdubal5@gmail.com>
2022-12-06 12:45:36 +0530

    first commit

--- Crm-test_app/tests/controller_test/update_controller.tests.js
+++ Crm-test_app/tests/controller_test/update_controller.tests.js
@@ -18,25 +18,47 @@ const userTestPayload = {
 
 describe('update', () => {
     it('Failure case', async() => {
-        //Arrange
-        const req = mockRequest();
-        const res = mockResponse();
-        req.params = { userId: '1' };
-        const spy = jest.sypOn(UserModel, 'findOneAndUpdate').mockImplementation(() => ({
-                exec: () => { throw new Error('Error Occurred') }
-            }))
-            //Act
-        await findAll(req, res);
+            //Arrange
+            const req = mockRequest();
+            const res = mockResponse();
+            req.params = { userId: '1' };
+            const spy = jest.sypOn(UserModel, 'findOneAndUpdate').mockImplementation(() => ({
+                    exec: () => { throw new Error('Error Occurred') }
+                }))
+                //Act
+            await update(req, res);
 
-        //Assert
-        expect(res.status).ToHaveBeenCalled(500);
-        expect(spy).ToHaveBeenCalled();
-        expect(res.send).ToHaveBeenCalledWith(
-            expect.objectContaining({
-                message: 'Some Error Occurred'
-            })
-        )
-    })
+            //Assert
+            expect(res.status).ToHaveBeenCalledWith(500);
+            expect(spy).ToHaveBeenCalled();
+            expect(res.send).ToHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: 'Some Error Occurred'
+                })
+            )
+        }),
+
+        it("Success case", async() => {
+            //Arrange
+            const req = mockRequest();
+            const res = mockResponse();
+            req.body.userId = "1";
+            const spy = jest.spyOn(update, "findOneAndUpdate").mockImplementation(() => ({
+                exec: jest.fn().mockReturnValue(Promise.resolve(userTestPayload))
+            }))
+
+            //Act
+            await update(req, res)
+
+            //Assert
+            expect(res.status).ToHaveBeenCalledWith(200);
+            expect(spy).ToHaveBeenCalled();
+            expect(res.send).ToHaveBeenCalledWith(
+                expect.objectContaining({
+                    message: 'Some Error Occurred'
+                })
+            )
+        })
 })
 
 //01:00:00
