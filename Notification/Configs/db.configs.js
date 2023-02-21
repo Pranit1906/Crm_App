8087ccd603e19dfd520c99a4c23b2ae645c8a571
Pranit <pranitdubal5@gmail.com>
2023-02-21 23:48:01 +0530

    first commit

---
+++ Notification/Configs/db.configs.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db_Name: 'Notification_App',
+    db_URL: 'mongodb://localhost/Notification_App'
+}
--- Notification/server.js
+++ Notification/server.js
@@ -2,20 +2,21 @@ require("./CronsJob/crons")
 const mongoose = require("mongoose");
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require("./Routes/notifi.routes")
+const route = require("./Routes/notifi.routes");
+const dbConfig = require('./Configs/db.configs');
 const app = express();
-const port = 3000;
+const serverConfig = require('./Configs/server');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/notification', { family: 4 }, (err) => {
+mongoose.connect(dbConfig.db_URL, { family: 4 }, (err) => {
     if (err) {
         console.log('Error Occurred')
     } else {
         console.log('Connected to DB');
-        app.listen(port, () => {
-            console.log(`App is listening at port ${port}`)
+        app.listen(serverConfig.PORT, () => {
+            console.log(`App is listening at port ${serverConfig.PORT}`)
         })
         app.use('/', route);
     }
